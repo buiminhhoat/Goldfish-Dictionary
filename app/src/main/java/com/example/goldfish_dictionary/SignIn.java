1dc8b630ccdb51677a0c301e4f0bed3bd3aee730
buiminhhoat <official.buiminhhoat@gmail.com>
2023-02-24 11:16:24 +0700

    Add Login with Google

--- app/src/main/java/com/example/goldfish_dictionary/SignIn.java
+++ app/src/main/java/com/example/goldfish_dictionary/SignIn.java
@@ -1,8 +1,13 @@
 package com.example.goldfish_dictionary;
 
+import static android.content.ContentValues.TAG;
+
+import android.accounts.Account;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,6 +23,14 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,7 +38,10 @@ import org.json.JSONObject;
 import java.util.Arrays;
 
 public class SignIn extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 200;
     Button btn_login_facebook;
+
+    Button btn_login_google;
     CallbackManager callbackManager;
 
     @Override
@@ -41,7 +57,7 @@ public class SignIn extends AppCompatActivity {
             public void onSuccess(LoginResult loginResult) {
                 System.out.println("onSuccess");
                 AccessToken accessToken = loginResult.getAccessToken();
-                getUserProfile(accessToken);
+                getUserProfileFacebook(accessToken);
             }
 
             @Override
@@ -62,9 +78,31 @@ public class SignIn extends AppCompatActivity {
                 LoginManager.getInstance().logInWithReadPermissions(SignIn.this, Arrays.asList("public_profile", "email"));
             }
         });
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        btn_login_google = findViewById(R.id.btn_login_google);
+        btn_login_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btn_login_google:
+                        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                        startActivityForResult(signInIntent, RC_SIGN_IN);
+                        break;
+                    // ...
+                }
+            }
+        });
     }
 
-    private void getUserProfile(AccessToken accessToken) {
+    private void getUserProfileFacebook(AccessToken accessToken) {
         /**
          Creating the GraphRequest to fetch user details
          1st Param - AccessToken
@@ -97,5 +135,30 @@ public class SignIn extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String email = account.getEmail();
+            String name = account.getDisplayName();
+            Uri picture = account.getPhotoUrl();
+            // Signed in successfully, show authenticated UI.
+//            updateUI(account);
+            System.out.println("Success");
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+//            updateUI(null);
+        }
     }
 }
