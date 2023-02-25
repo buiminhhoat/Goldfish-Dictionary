fd338232060cf1dfaa965da56d869f228c1baa07
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-02-25 17:30:26 +0700

    Add Dictionary Activity

---
+++ app/src/main/java/com/example/goldfish_dictionary/SignUp.java
@@ -0,0 +1,12 @@
+package com.example.goldfish_dictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUp extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
