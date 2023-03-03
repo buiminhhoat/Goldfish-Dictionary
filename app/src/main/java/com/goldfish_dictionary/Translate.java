f9caf92fd4495aa3f42343e8f72201456f38d10e
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-03-04 04:52:37 +0700

    Add Translate Activity

---
+++ app/src/main/java/com/goldfish_dictionary/Translate.java
@@ -0,0 +1,12 @@
+package com.goldfish_dictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Translate extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translate);
+    }
+}
