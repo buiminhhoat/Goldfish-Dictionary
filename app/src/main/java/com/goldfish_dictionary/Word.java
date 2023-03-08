b54a9956eba88e12ea678bd134d303acd1ab64b7
buiminhhoat <official.buiminhhoat@gmail.com>
2023-03-08 11:11:30 +0700

    Add Notification

--- app/src/main/java/com/goldfish_dictionary/Word.java
+++ app/src/main/java/com/goldfish_dictionary/Word.java
@@ -1,9 +1,17 @@
 package com.goldfish_dictionary;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.core.app.NotificationCompat;
+
+import java.util.Date;
 
 public class Word extends Activity {
     private String word;
@@ -28,6 +36,10 @@ public class Word extends Activity {
 
         TextView tv_meaning = findViewById(R.id.txt_meaning);
         tv_meaning.setText(dataBaseHelper.getVocabulary(word).getMeaning());
+
+        createNotificationChannel();
+        createNotification("Goldfish Dictionary", word);
+
     }
 
     private void initializationDatabase() {
@@ -41,5 +53,32 @@ public class Word extends Activity {
         dataBaseHelper = new DatabaseHelper(Welcome.this, "vi_fr.db");
         dataBaseHelper.createDatabase();
         */
+    }
+
+    public void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Goldfish Dictionary";
+            String description = "Goldfish Dictionary";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Goldfish Dictionary", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system. You can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void createNotification(String contentTitle, String contentText) {
+        android.app.Notification notification = new NotificationCompat.Builder(this, "Goldfish Dictionary")
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setSmallIcon(R.drawable.icon)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify((int) new Date().getTime(), notification);
     }
 }
