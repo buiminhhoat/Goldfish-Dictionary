d3f5fad515fb4969cf24ff6e0d674e9ed3be1c4d
buiminhhoat <official.buiminhhoat@gmail.com>
2023-03-13 13:10:03 +0700

    Update Constants

--- app/src/main/java/com/goldfish_dictionary/Constants.java
+++ app/src/main/java/com/goldfish_dictionary/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static String IP = "goldfish-dictionary.buiminhhoat.com";
     public static String PORT = "3306";
     public static String DATABASE = "goldfish_dictionary";
-    public static String DATABASE_URL = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
+    public static String DATABASE_URL = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE + "?useUnicode=true&characterEncoding=UTF-8";
     public static String USER_NAME = "goldfish_dictionary";
     public static String PASSWORD = "goldfish_dictionary";
 }
